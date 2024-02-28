7ffeb4a870432b39506487af51123d5b372e0edf
louis-demetz <61187384+louis-demetz@users.noreply.github.com>
2024-02-28 20:14:19 +0100

    arguments check is in a separae function

--- src/main.c
+++ src/main.c
@@ -3,24 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lodemetz <lodemetz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: louis.demetz <louis.demetz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 20:34:42 by louis.demet       #+#    #+#             */
-/*   Updated: 2024/02/28 13:07:15 by lodemetz         ###   ########.fr       */
+/*   Updated: 2024/02/28 20:13:56 by louis.demet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+int	check_arguments(int argc)
+{
+	if (argc < 5 || argc > 6)
+	{
+	printf("args error: expected:\n"
+		"- nb_philosophers\n"
+		"- ms_to_starve\n"
+		"- ms_to_eat\n"
+		"- ms_to_sleep\n"
+		"- times_eating (optional)\n");
+		return (EXIT_FAILURE);
+	}
+	return (SUCCESS);
+}
+
 int	main(int argc, char **argv)
 {
 	t_data	*data;
 
-	if (argc < 5 || argc > 6)
-	{
-		printf("args error: expected:\n- nb_philosophers\n- ms_to_starve\n- ms_to_eat\n- ms_to_sleep\n- times_eating (optional)\n");
+	if (!check_arguments(argc))
 		return (EXIT_FAILURE);
-	}
 	data = malloc(sizeof(t_data));
 	if (!data)
 		return (ft_error(data, 1));
