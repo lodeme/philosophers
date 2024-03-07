c3f050a3c693b6be57d1a94adaaecfe3a50d06ce
lodemetz <lodemetz@student.42berlin.de>
2024-03-07 17:35:24 +0100

    added philo project

---
+++ philo/src/main.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: piuser <piuser@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/26 20:34:42 by louis.demet       #+#    #+#             */
+/*   Updated: 2024/03/04 01:01:16 by piuser           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int	check_arguments(int argc)
+{
+	if (argc < 5 || argc > 6)
+	{
+		printf("args error: expected:\n"
+			"1. (int) nb_philosophers\n"
+			"2. (int) ms_to_starve\n"
+			"3. (int) ms_to_eat\n"
+			"4. (int) ms_to_sleep\n"
+			"5. (int) times_eating (optional)\n");
+		return (FAILURE);
+	}
+	return (SUCCESS);
+}
+
+int	main(int argc, char **argv)
+{
+	t_data	*data;
+
+	if (!check_arguments(argc))
+		return (EXIT_FAILURE);
+	data = malloc(sizeof(t_data));
+	if (!data)
+		return (ft_error(data, 1));
+	if (!init_data(data, argv))
+		return (EXIT_FAILURE);
+	if (!philosophers(data))
+		return (EXIT_FAILURE);
+	free_data(data);
+	return (EXIT_SUCCESS);
+}
---
+++ philo/src/utils.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: piuser <piuser@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/27 15:58:41 by louis.demet       #+#    #+#             */
+/*   Updated: 2024/03/04 08:48:54 by piuser           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int	ft_atoi(const char *str)
+{
+	int	result;
+	int	sign;
+
+	result = 0;
+	sign = 1;
+	while (*str == ' ' || *str == '\t' || *str == '\n'
+		|| *str == '\v' || *str == '\f' || *str == '\r')
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	return (sign * result);
+}
+
+long long	ts(void)
+{
+	struct timeval	time;
+
+	gettimeofday(&time, 0);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
+}
+
+long long	ms_elapsed(t_data *data)
+{
+	return (ts() - data->ts_start);
+}
+
+int	access_mutex(int *var, pthread_mutex_t *mutex)
+{
+	int	value;
+
+	pthread_mutex_lock(mutex);
+	value =	*var;
+	pthread_mutex_unlock(mutex);
+	return (value);
+}
